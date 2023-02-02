69a86a25aecca41ff1b94082900519ab77d6368f
Declan Cross <crossdecl@ecs.vuw.ac.nz>
2023-02-02 17:28:13 +1300

    css and styles

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'./public/index.html'));
 });
 
-server.use('/api/worklogs', worklogs)
+// app.use('/api/worklogs', worklogs)
 
 app.listen(3000,  () => console.log("Example app listening on port 3000!"));
