655417a623ff18bbd0d9cc5bdeeb8cb07aa6a2fc
Karl Bennett <karl.bennett@techtime.co.nz>
2023-02-03 11:52:43 +1300

    Add worklog removal commands

--- worklogs.js
+++ worklogs.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const fs = require('fs');
-const { DateTime } = require('luxon')
 
 module.exports = router
 
@@ -20,12 +19,35 @@ router.post('/', (request, response) => {
             response.sendStatus(500);
             return;
         }
-        let object = JSON.parse(data.toString());
+        let worklogs = JSON.parse(data.toString());
+        if(request.body.title === 'DELETE_ALL'){
+            fs.writeFile('./data.json', JSON.stringify([]), err => {
+                if(err){
+                    console.log(err)
+                    response.sendStatus(500)
+                } else{
+                    console.log("Data cleared")
+                }
+            })
+            return
+        }
         
-        object = object.filter(o => o.minutes !== request.body.minutes)
         console.log(request)
-        object.push({title: request.body.title, minutes: request.body.minutes})
-        fs.writeFile('./data.json', JSON.stringify(object), err => {
+        if(request.body.title === 'DELETE_LAST'){
+            worklogs.pop()
+            fs.writeFile('./data.json', JSON.stringify(worklogs), err => {
+                if(err){
+                    console.log(err)
+                    response.sendStatus(500)
+                } else{
+                    console.log("Data saved")
+                }
+            })
+            return;
+        }
+        worklogs = worklogs.filter(o => o.minutes !== request.body.minutes)
+        worklogs.push({title: request.body.title, minutes: request.body.minutes})
+        fs.writeFile('./data.json', JSON.stringify(worklogs), err => {
             if(err){
                 console.log(err)
                 response.sendStatus(500)
